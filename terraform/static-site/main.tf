6502b22811aa151eb99a812ebdc0ab35416c5549
rodrigo <rodrigoquijarro@gmail.com>
2021-06-21 00:03:40 +0300

    init

--- terraform/static-site/main.tf
+++ terraform/static-site/main.tf
@@ -2,18 +2,101 @@
 # 1. Configure the AWS Provider
 provider "aws" {
     region = var.region
-    #shared_credentials_file = "/var/lib/jenkins/.aws/credentials"
-    profile = var.profile
+    shared_credentials_file = "/var/lib/jenkins/.aws/credentials"
+    #profile = var.profile
 }
 
+data "aws_availability_zones" "all" {}
+
 resource "aws_instance" "web" {
-    ami = “${lookup(var.amis,var.region)}”
-    count = “${var.count}”
-    instance_type = "t2.micro"
+    ami = "${lookup(var.amis,var.region)}"
+    count = "${var.ec2_count}"
     key_name = "apptest"
-    vpc_security_group_ids = ["sg-0066adba3fba96c74"]
+    vpc_security_group_ids = ["${aws_security_group.instance.id}"]
+    source_dest_check = false
+    instance_type = "t2.micro"
         tags = {
-            Name = var.name
+            Name = "${format("web-%03d", count.index + 1)}"
             group = var.group
+  }
+}
+### Creating Security Group for EC2
+resource "aws_security_group" "instance" {
+  name = "terraform-instance"
+  ingress {
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+## Creating Launch Configuration
+resource "aws_launch_configuration" "example" {
+  image_id               = "${lookup(var.amis,var.region)}"
+  instance_type          = "t2.micro"
+  security_groups        = ["${aws_security_group.instance.id}"]
+  key_name               = "${var.key_name}"
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+## Creating AutoScaling Group
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = "${aws_launch_configuration.example.id}"
+  availability_zones = ["${data.aws_availability_zones.all.names[0]}"]
+  min_size = 2
+  max_size = 10
+  load_balancers = ["${aws_elb.example.name}"]
+  health_check_type = "ELB"
+  tag {
+    key = "Name"
+    value = "terraform-asg-example"
+    propagate_at_launch = true
+  }
+}
+## Security Group for ELB
+resource "aws_security_group" "elb" {
+  name = "terraform-example-elb"
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+### Creating ELB
+resource "aws_elb" "example" {
+  name = "terraform-asg-example"
+  security_groups = ["${aws_security_group.elb.id}"]
+  availability_zones = ["${data.aws_availability_zones.all.names[0]}"]
+  health_check {
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+    timeout = 3
+    interval = 30
+    target = "HTTP:8080/"
+  }
+  listener {
+    lb_port = 80
+    lb_protocol = "http"
+    instance_port = "8080"
+    instance_protocol = "http"
   }
 }
--- terraform/static-site/variables.tf
+++ terraform/static-site/variables.tf
@@ -1,21 +1,26 @@
-variable "profile" {
-    description = "The profile used to auth to AWS"
-}
+# variable "profile" {
+#     description = "The profile used to auth to AWS"
+# }
 
 variable "region" {
     description= "The region our instance will be in (exmpl: eu-west-2)"
     default = "eu-west-2"
 }
 
-variable "name" {
-    description= "The name of the instance we are creating"
-}
+# variable "name" {
+#     description= "The name of the instance we are creating"
+# }
 
 variable "group" {
     description= "the name of the group we will be using for Ansible purposes"
 }
 
-variable "count" {
+variable "key_name" {
+  description = "Key name for SSHing into EC2"
+  default = "apptest"
+}
+
+variable "ec2_count" {
     default = 1
 }
 
