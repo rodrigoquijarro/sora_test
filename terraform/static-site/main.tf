dc7c35591900c96bbaed659166e06e47fbdf2f0e
rodrigo <rodrigoquijarro@gmail.com>
2021-06-20 11:07:52 +0300

    init

--- terraform/static-site/main.tf
+++ terraform/static-site/main.tf
@@ -121,11 +121,26 @@ resource "aws_eip" "one" {
   }
 }
 
+# resource "aws_instance" "site" {
+#     ami = "ami-0194c3e07668a7e36"
+#     instance_type="t2.micro"
+#     key_name="apptest"
+#     vpc_security_group_ids = ["sg-0066adba3fba96c74"]
+#         tags = {
+#             Name = var.name
+#             group = var.group
+#         }
+# }
+
 resource "aws_instance" "site" {
     ami = "ami-0194c3e07668a7e36"
     instance_type="t2.micro"
     key_name="apptest"
-    vpc_security_group_ids = ["sg-0066adba3fba96c74"]
+#    vpc_security_group_ids = ["sg-0066adba3fba96c74"]
+    network_interface {
+      device_index = 0
+      network_interface_id = aws_network_interface.web-server-nic.id
+    }
         tags = {
             Name = var.name
             group = var.group
