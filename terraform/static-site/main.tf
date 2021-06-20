b3f35e5befa8e4335cec0f1d0c478c2d55e8a0f1
rodrigo <rodrigoquijarro@gmail.com>
2021-06-20 19:33:44 +0300

    init

--- terraform/static-site/main.tf
+++ terraform/static-site/main.tf
@@ -122,3 +122,14 @@ resource "aws_eip" "one" {
             Name = "Enviroment_Elastic_IP"
         }
 }
+
+resource "aws_instance" "site" {
+    ami = "ami-0194c3e07668a7e36"
+    instance_type = "t2.micro"
+    key_name = "apptest"
+    vpc_security_group_ids = ["sg-0066adba3fba96c74"]
+        tags = {
+            Name = var.name
+            group = var.group
+  }
+}
