0bdc22c63b8eda8a9e41bc29cca0d4db0c80ad12
rodrigo <rodrigoquijarro@gmail.com>
2021-06-20 17:04:38 +0300

    init

--- terraform/static-site/variables.tf
+++ terraform/static-site/variables.tf
@@ -2,9 +2,9 @@ variable "profile" {
     description = "The profile used to auth to AWS"
 }
 
-variable "region" {
-    description= "The region our instance will be in (exmpl: eu-west-2)"
-}
+# variable "region" {
+#     description= "The region our instance will be in (exmpl: eu-west-2)"
+# }
 
 variable "name" {
     description= "The name of the instance we are creating"
@@ -12,4 +12,33 @@ variable "name" {
 
 variable "group" {
     description= "the name of the group we will be using for Ansible purposes"
+}
+
+variable "region" {
+    type = string
+    description = "AWS East Region"
+}
+
+variable "azs" {
+    type = list
+    description = "Availability Zones"
+}
+
+variable "subnet_cidr" {
+    type = list
+    description = "VPC Subnet CIDR Ranges"
+}
+
+variable "ami" {
+    type = string
+    description = "Ubuntu AMI ID in N. California Region"
+}
+
+variable "instance_type" {
+    type = string
+    description = "Instance type"
+}
+
+output "lb_dns" {
+    value = aws_lb.alb.dns_name
 }
