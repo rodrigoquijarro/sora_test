0375dc1bc8e444d089d95e9702b7faef38cfcfed
rodrigo <rodrigoquijarro@gmail.com>
2021-06-21 21:29:56 +0300

    init

--- terraform/static-site/main.tf
+++ terraform/static-site/main.tf
@@ -60,11 +60,6 @@ resource "aws_launch_configuration" "timeisweb" {
   instance_type          = "t2.micro"
   security_groups        = ["${aws_security_group.instance.id}"]
   key_name               = "${var.key_name}"
-  user_data = <<-EOF
-              #!/bin/bash
-              echo "Hello, World" > index.html
-              nohup busybox httpd -f -p 8080 &
-              EOF
   lifecycle {
     create_before_destroy = true
   }
