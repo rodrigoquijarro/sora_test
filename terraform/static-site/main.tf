b709a40952307c6f27bbdfc596bc6d8850372398
rodrigo <rodrigoquijarro@gmail.com>
2021-06-22 12:11:20 +0300

    init

--- terraform/static-site/main.tf
+++ terraform/static-site/main.tf
@@ -71,7 +71,7 @@ resource "aws_autoscaling_group" "timeisweb" {
   min_size = 2
   max_size = 4
   load_balancers = ["${aws_elb.timeisweb.name}"]
-  health_check_grace_period = 300
+  health_check_grace_period = 600
   health_check_type = "ELB"
   tag {
     key = "Name"
