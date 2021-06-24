b65cc205700455c8cb2fcdf2afd49019d838a2ab
rodrigo <rodrigoquijarro@gmail.com>
2021-06-24 09:07:18 +0300

    init

--- terraform/static-site/main.tf
+++ terraform/static-site/main.tf
@@ -31,13 +31,13 @@ resource "aws_security_group" "instance" {
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  ingress {
-    description = "HTTP from VPC" 
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#   ingress {
+#     description = "HTTP from VPC" 
+#     from_port   = 80
+#     to_port     = 80
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
   ingress {
     description = "Shell from VPC"  
     from_port   = 22
